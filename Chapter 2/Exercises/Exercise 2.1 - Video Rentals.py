851dd001603cdf6e55c7421d107774fb3e81e1cf
ydrubs <ydrubins@yahoo.com>
2024-08-29 15:27:18 -0500

    First commit

---
+++ Chapter 2/Exercises/Exercise 2.1 - Video Rentals.py
@@ -0,0 +1,15 @@
+"""
+Exercise 2.1 - Video Rentals
+
+Five Star Retro Video rents VHS tapes and DVDs to the same connoisseurs who like to buy LP record albums.
+
+The store rents new videos for $3.00 a night, and oldies for $2.00 a night.
+
+Write a program that the clerks at Five Star Retro Video can use to calculate the total charge for a customer’s video rentals.
+
+The program should prompt the user for the number of each type of video and output the total cost.
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
+
