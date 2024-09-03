48b45b273934451939af2667289634674be0e282
ydrubs <ydrubins@yahoo.com>
2024-09-03 16:20:10 -0500

    initial commit

---
+++ Chapter 2/Exercises/Exercise 2.2 - Spheres.py
@@ -0,0 +1,10 @@
+"""
+Exercise 2.2 - Spheres
+
+Write a program that takes the radius of a sphere (a floating-point number) as input
+
+and then outputs the sphere’s diameter, circumference, surface area, and volume (feel free to google the formulas).
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
