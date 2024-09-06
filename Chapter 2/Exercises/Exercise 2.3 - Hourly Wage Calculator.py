6f7b16362cbaf6bc7d8c51d97aa291023b59c8e4
ydrubs <ydrubins@yahoo.com>
2024-09-06 08:12:43 -0500

    initial commit

---
+++ Chapter 2/Exercises/Exercise 2.3 - Hourly Wage Calculator.py
@@ -0,0 +1,15 @@
+"""
+Exercise 2.3 - Hourly Wage Calculator
+
+An employee’s total weekly pay equals the hourly wage multiplied by the total number of regular hours
+
+plus any overtime pay. Overtime pay equals the total overtime hours multiplied by 1.5 times the hourly wage.
+
+Write a program that takes as inputs the hourly wage, total regular hours, and total overtime hours
+
+and displays an employee’s total weekly pay. All values should be floats.
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
+
