d3556efff02b4ecbf545bfc0fb3ca813de9295ed
ydrubs <ydrubins@yahoo.com>
2024-09-25 09:30:21 -0500

    initial Commit

---
+++ Chapter 3/Exercises/Exercise 3.2a – Pyramid Number Problem.py
@@ -0,0 +1,24 @@
+"""
+Exercise 3.2a – Pyramid Number Problem
+
+A pyramidal number has the form 1^2+2^2+3^2+.......
+
+for example the forth pyramidal number is 30 because:
+    1^2+2^2+3^2^4^2 = 1+4+9+16=30
+
+It is also known as the cannonball stacking number becasue it represents how much spherical cannonballs
+can be stacked in a pyramid shape with a given number of rows.
+
+Your program should:
+
+1) Ask the user how many rows of cannonballs they want to stack
+
+2) Calculate the total number of cannonballs that will create the pyramid with the specified rows
+
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
+###YOUR SOLUTION HERE
+
+
---
+++ Chapter 3/Exercises/Exercise 3.2b - Bounce Distance.py
@@ -0,0 +1,25 @@
+
+"""
+Exercise 3.2b - Bounce Distance
+
+A standard science experiment is to drop a ball and see how high it bounces.
+Once the "bounciness" of the ball has been determined, the ratio gives a bounci-ness index.
+
+For example, if a ball dropped from a height of 10 feet, and the first bound goes to 6 feet,
+we can say the bounce index is 0.6 (6/10) and the total distance traveled by the ball is 16 feet after one bounce (10 ft + 6ft)
+
+If the ball were to continue bouncing, the distance after two bounces
+would be 10 ft + 6 ft + 6ft + 3.6 ft = 25.6 ft.
+
+Note, in the example here, the distance traveled for
+each successive bounce is the distance to the floor plus 0.6 of that distance as the ball comes back up.
+
+Write a program that lets the user enter the initial height from which the ball is dropped, the height after the first bounce,
+and the number of times the ball is allowed to continue bouncing. Output should be the total distance traveled by the ball.
+
+
+
+    :Author: <NAME>
+    :Date: <TODAY'S DATE>
+"""
+###YOUR SOLUTION HERE
