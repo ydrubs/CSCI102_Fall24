1b4460a16958c4255203cb5e71e306994b280811
ydrubs <ydrubins@yahoo.com>
2024-11-11 09:32:20 -0600

    11/11 update

--- Chapter 5/ch5_CODE_in_class.py
+++ Chapter 5/ch5_CODE_in_class.py
@@ -147,41 +147,54 @@ random_student = random.choice(csci102_Roster)
 
 
 ##Slide 13 - Changing elements in a list
-first = [1,2,3,4]
-first[0] = 5
-print(first)
+# first = [1,2,3,4]
+# first[0] = 5
+# print(first)
 
-csci102_Roster[0] = 'Taylor Swift'
-print(csci102_Roster)
+# csci102_Roster[0] = 'Taylor Swift'
+# print(csci102_Roster)
 
 
 ##Slide 14 - Manipulating through looping
 # numbers = [2,3,4,5]
-# pass
-#     pass
+# for i in range(len(numbers)):
+#     numbers[i] = numbers[i] ** 2
+#
 # print(numbers)
-
+#
 # sentence = "Python is a good beginner programming language"
 # sentence = sentence.split() #Split into individual words and make a list
+# print(sentence)
 # print(f"The sentence is {len(sentence)} words.")
 
-#pass #Loop through each element by index
-#     pass #Change each word to upper case and save it into the same position
+# for i in range(len(sentence)): #Loop through each element by index
+#     print(sentence[i].isupper())
+#     sentence[i] = sentence[i].upper() #Change each word to upper case and save it into the same position
+
+# print(sentence)
+
+# for i, word in enumerate(sentence):
+#     sentence[i] = word.upper()
+
 # print(sentence)
 
 
 ##Slide 15 - adding elements by index
-# pass
-# pass
+# csci102_Roster[1] = 'Kanye'
 # print(csci102_Roster)
-
-# pass
+# csci102_Roster.insert(0,'Eliud')
 # print(csci102_Roster)
 
 
-# new_students = ['Larry', 'Curley', 'Moe']
-# pass
+# new_students = ['Stewie G.', 'Yuriy', 'Brendan Flowers']
+# csci102_Roster.extend(new_students)
 # print(csci102_Roster)
+#
+# csci102_Roster = csci102_Roster + ['James Hatfield', 'Chritian Bale']
+# print(csci102_Roster)
+
+
+
 
 ##Slide 16 - removing elements
 # pass
@@ -193,22 +206,29 @@ print(csci102_Roster)
 # print(csci102_Roster)
 
 ##Remove an element using its name
-# pass
-# pass
-# print(pos, new_freind)
+# last_person = csci102_Roster.pop()
+# print(last_person)
+# a_person = csci102_Roster.pop(random.randint(0,20))
+# print(a_person)
+#
+
+# freind = csci102_Roster.index('Javier')
+# print(freind)
+# extra_creidt = csci102_Roster.pop(csci102_Roster.index('Javier'))
 
 ##Slide 19 - searching and sorting
 ##Building a list from conditions in another list (numbers greater then 15)
-# from random import randint
+from random import randint
 #
 # lst = []
 # for i in range(20):
 #     lst.append(randint(1,20))
+# print(lst)
 #
 # new_lst = []
-# pass
-#     pass
-#         pass
+# for n in lst:
+#     if n > 15:
+#         new_lst.append(n)
 #
 # print(new_lst)
 
@@ -216,17 +236,23 @@ print(csci102_Roster)
 ##Given a list of 20 random integers between 1 and 20, create a new list of all integers that appear more then once.
 # from random import randint
 #
-# lst = []
-# for i in range(20):
-#     lst.append(randint(1,20))
-#
+lst = []
+for i in range(20):
+    lst.append(randint(1,20))
+# print(lst)
+
+
 # new_lst = []
-# pass
-#     pass
-#         pass
+# for n in lst: # Check each element in the original
+#     if lst.count(n) > 1: # If that element appears more then once
+#         new_lst.append(n) # Move THAT element to a new list
+
+# print(new_lst)
+# new_lst = set(new_lst)
+# print(new_lst)
 #
-# print(pass)
-# print(pass)
+# new_lst = list(new_lst)
+# # print(new_lst)
 
 ##Given TWO lists of random numbers between 1 and 20 create a new list that has the common elements of both lists
 # lst2 = []
@@ -235,23 +261,27 @@ print(csci102_Roster)
 #
 # print(lst)
 # print(lst2)
-#
-# new_lst = []
-# pass
-#     pass
-#         pass
-#         pass
-#
+
+new_lst = []
+# for n in lst:
+#     if n in lst2:
+#         new_lst.append(n)
+#         lst2.pop(lst2.index(n))
+# #
 # print(new_lst)
+
 
 ##Slide 21 - Sorting a list
 # lst = [4,10,2,1,7]
 #
-# pass
+# lst.sort() #in-place sorting
 # print(lst)
-#
+# #
 # lst2 = [4,10,2,1,7]
-#
+# lst_2 = sorted(lst2)
+# print(lst2)
+# print(lst_2)
+
 # print(pass) # This returns the list sorted in numeric order
 # print(lst2) #Notice that this returns the original list - not the sorted one
 #
@@ -262,36 +292,60 @@ print(csci102_Roster)
 
 
 
+
+
 ##Slide 23 - Intro to Tuples
-# cards = pass #Empty tuple container
-# cards = (6,7,8,9,10,'Jack') #Tuples of card ranks
+cards = () #Empty tuple container
+cards = (6,7,8,9,10,'Jack') #Tuples of card ranks
 # cards = ((2,'hearts'), (8, 'spades'), ('Ace', 'Clubs')) #Tuple of tuples
-# cars = ([2020, 'Chevy', 'Corvette'], [2023, 'Toyota', 'Prius']) #Tuple of lists
+cars = ([2020, 'Chevy', 'Corvette'], [2023, 'Toyota', 'Prius']) #Tuple of lists
 
+# print(cards[0])
+# cards[0] = 'King' #Demonstrates you can't modify a tuple like you can a list
+# print(cards)
 
-# pass #Demonstrates you can't modify a tuple like you can a list
-
-# pass #Since the third element [2] in the first tuple is a list, it can be modified. We are not modifying the tuple itself
+# cars[0][1] = 'Ford'
+# cars[0][2] = 'Mustang GT 500'
 # print(cars)
+
 
 
 ##SLide 25
 ###Building a deck of cards by combining tuples
-# import random
-#
-# suit_tuple= ('hearts', 'diamomds', 'spades', 'clubs')
-# rank_tuple = ('2', '3', '4', '5', '6', '7', '8','9', '10', 'jack', 'queen', 'king', 'ace')
-#
-# deck = []
-# pass
-#     pass
-#         pass #Temporary variable to store the current card in loop
-#         pass #Add this card to the deck list
-#         pass
-#
-# print(deck)
+import random
+
+suit_tuple= ('hearts', 'diamomds', 'spades', 'clubs')
+rank_tuple = ('2', '3', '4', '5', '6', '7', '8','9', '10', 'jack', 'queen', 'king', 'ace')
+
+deck = []
+for suit in suit_tuple:
+    for rank in rank_tuple:
+        card = (rank, suit) #Temporary variable to store the current card in loop
+        deck.append(card) #Add this card to the deck list
+
+print(deck)
+print(deck[random.randint(0,len(deck))])
+
+random.shuffle(deck)
+print(deck)
 
 ##Slide 26 (IN_CLASS_TUPLE PRACTICE)
+point1 = (6,6)
+point2 = (9,1)
+
+x1 = point1[0]
+y1 = point1[1]
+
+x2 = point2[0]
+y2 = point2[1]
+
+slope = (y2-y1)/(x2-x1)
+print(slope)
+
+slope_alt = (point2[1]-point1[1]/point2[0]-point1[0])
+
+
+
 
 
 ##Slide 28
