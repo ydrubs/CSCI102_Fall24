e5eb010bc561a8bda3cdc8428192381a3dab0900
ydrubs <ydrubins@yahoo.com>
2024-11-18 09:36:40 -0600

    initial commit

---
+++ Chapter 5/Exercises/Exercise 5.4 - Palindrome Checker.py
@@ -0,0 +1,13 @@
+"""
+Exercise 5.4 - Palindromes
+
+Palindromes – A palindrome is a word that reads the same forward and backward. Some examples are:
+        - civic, radar, level, rotor, kayak, madam, and refer.
+
+Write a function called palindrome_checker that accepts a one word string and returns whether the word is a palindrome.
+
+The checker should NOT be case-sensitive so civic == Civic == CIVIC
+
+Demonstrate the functionality of the Palindrome checker by calling the function with the argument 'Bob'.
+"""
+# YOUR SOLUTION HERE
